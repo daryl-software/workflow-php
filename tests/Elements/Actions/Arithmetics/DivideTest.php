f30f1506a43adc62a7eb98532c869f3171c06b92
Alex Zecca <alexzecca@msn.com>
2020-04-28 09:38:53 +0000

    fix divide test

---
+++ tests/Elements/Actions/Arithmetics/DivideTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ezweb\Workflow\Test\Elements\Action\Arithmetics;
+
+use Ezweb\Workflow\Providers\Provider;
+
+class DivideTest extends \PHPUnit\Framework\TestCase
+{
+    private \Ezweb\Workflow\Test\Mocker\ScalarMocker $scalarBuilder;
+
+    protected function setUp(): void
+    {
+        $this->scalarBuilder = new \Ezweb\Workflow\Test\Mocker\ScalarMocker();
+    }
+
+    /**
+     * @dataProvider argDatasetProvider
+     */
+    public function testIsValidWithDifferentArgs($args, $expected)
+    {
+        $divide = \Ezweb\Workflow\Elements\Actions\Arithmetics\Divide::create();
+        foreach ($args as $arg) {
+            $scalarMock = $this->scalarBuilder->getMockWithValue($arg);
+            $divide->addArgs($scalarMock);
+        }
+        $method = new \ReflectionMethod($divide, 'isValid');
+        $method->setAccessible(true);
+
+        $this->assertSame($expected, $method->invokeArgs($divide, [[], $args]));
+    }
+
+    public function argDatasetProvider()
+    {
+        return [
+            [[6, 2], true]
+        ];
+    }
+}
